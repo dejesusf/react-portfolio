91d55f7b2256c86e9b36c03b903ccb5c91f5a305
dejesusf <dejesusf@uw.edu>
2023-03-02 11:59:53 -0800

    fixed index js to use createRoot instead of ReactDom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import Footer from './components/Footer';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Footer />, document.querySelector('footer'));
+const main = document.getElementById('root');
+const footerContainer = document.querySelector('footer');
+
+createRoot(main).render(<App />);
+createRoot(footerContainer).render(<Footer />);
