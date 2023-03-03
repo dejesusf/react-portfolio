48b05811d5239fb96092278e12b6bdf42cd9e311
dejesusf <dejesusf@uw.edu>
2023-03-03 10:50:41 -0800

    added screenshot images for portoflio and fixed code

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,4 +1,52 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const projects = [
+  {
+    id: uuidv4(),
+    title: "Anoma Drama Llama",
+    image: "/images/anoma drama llama.png",
+    altText: "Customize your llama to suit your drama.",
+    description: "Anoma Drama Llama allows users to choose how public they want to display their thoughts and emotions. The user is able to set a current emotion or post journals using multiple emotions. These moods can be tracked hourly, daily, and even yearly. All posts default to private because this journal intimately belongs to the user and they have all the power on what they allow people to see.",
+    tech: "This project utilizes MySQL, Handlebars, Nodemailer, Plotly, and Ninja Quotes API.",
+    contributers: "This was a group project. Creators and their github profiles can be found in the repository link below.",
+    link: "https://anomallama.herokuapp.com/",
+    repoLink: "https://github.com/dejesusf/anoma-drama-llama"
+  },
+  {
+    id: uuidv4(),
+    title: "Moves & Brews",
+    image: "/images/moves and brews.png",
+    altText: "Text",
+    description: "Moves & Brews would allow for the user to search a city and the page will populate with nearby breweries and events.",
+    tech: "This application uses HTML, CSS, JavaScript, and two APIs: OpenBrewery and TicketMaster.",
+    contributers: "This was originally a group project. You can find the other creators in the repository link below. I've recently taken the time to update the webpage aesthetics to match my style of webpages.",
+    link: "https://dejesusf.github.io/moves-and-brews",
+    repoLink: "https://github.com/dejesusf/moves-and-brews"
+  },
+  {
+    id: uuidv4(),
+    title: "ReadMe Generator",
+    image: "/images/professional readme gen.png",
+    altText: "An example of a generated readMe using this application",
+    description: "For this assignment, we were given the task of generating a markdown file with the use of user inputs straight from the command line.",
+    tech: "This uses 100% JavaScript with the help of node.js and inquirer.",
+    contributers: "This was a single user assignment and I was the only contributer.",
+    link: "https://github.com/dejesusf/readme-generator",
+    repoLink: "https://github.com/dejesusf/readme-generator"
+  },
+  {
+    id: uuidv4(),
+    title: "SQL: Employee Tracker",
+    image: "/images/orm ecomm.png",
+    altText: "Database query shown in VSCode",
+    description: "This assignment focuses on the use of SQL, specifically MySQL, to create a database of employees for a given company. This application also allows user inputs because of the inquirer npm package.",
+    tech: "This terminal command-line application uses MySQL.",
+    contributers: "This was a single user assignment and I was the only contributer.",
+    link: "https://github.com/dejesusf/sql-employee-tracker",
+    repoLink: "https://github.com/dejesusf/sql-employee-tracker"
+  }
+]
 
 export default function Portfolio() {
   return (
@@ -13,16 +61,25 @@ export default function Portfolio() {
       
       <div className="row">
         <div className="column">
-          <div className="middle g1-block">
-            <div className="small-left" id="image">
-              <a href=""><img src="" /></a>
-            </div>
+          {projects.map((element) => {
+              return (
+                <div key={element.id}className="middle g1-block">
+                  <div className="small-left" id="image">
+                    <a href={element.link} target="_blank" rel="noopener noreferrer">
+                      <img src={element.image} alt={element.altText} />
+                    </a>
+                  </div>
 
-            <div className="large-right">
-              <h2>PROJECT NAME</h2>
-              <p>Hi! Welcome to my porfolio that was built using React! Please navigate using the links above to learn About Me and to see my Portfolio. If you like what you see, feel free to contact me via the Contact page.</p>
-            </div>
-          </div>
+                  <div className="large-right">
+                    <h2>{element.title}</h2>
+                    <p>{element.description}</p>
+                    <p>{element.tech}</p>
+                    <p>{element.contributers}</p>
+                    <p><strong>Link to Repository: </strong><a href={element.repoLink} target="_blank" rel="noopener noreferrer">GitHub</a></p>
+                  </div>
+                </div>
+              )
+            })}
         </div>
       </div>
     </main>
