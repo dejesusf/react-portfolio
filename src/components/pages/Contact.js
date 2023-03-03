2f6ec68ffba3c67db2986016cd9690ff0e6cc3d1
dejesusf <dejesusf@uw.edu>
2023-03-03 12:37:35 -0800

    created contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function Contact() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  function handleChange(e) {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert(`Thank you, ${name}, for your submission! At this point in time, it will not be received as the backend code for the form has not yet been created.`)
+
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
   return (
     <main>
       <div className="row">
@@ -14,8 +41,19 @@ export default function Contact() {
       <div className="row">
         <div className="column">
           <div className="middle">
-            {/* TODO: ADD FORM */}
-            <p>Hi! Welcome to my porfolio that was built using React! Please navigate using the links above to learn About Me and to see my Portfolio. If you like what you see, feel free to contact me via the Contact page.</p>
+            <p>Hi! If you'd like to contact me, please fill out the form below.</p>
+            <form onSubmit={handleFormSubmit}>
+              <label htmlFor='name'>Name: </label>
+              <input type='text' placeholder="Name" name="name" onChange={handleChange} value={name} />
+              <br />
+              <label htmlFor='email'>Email: </label>
+              <input type='email' placeholder="Email" onChange={handleChange} value={email} name="email" />
+              <br />
+              <label htmlFor='message'>Message: </label>
+              <input type='text' placeholder="Message" onChange={handleChange} value={message} name="message" />
+              <br />
+              <button type="submit">Submit</button>
+            </form>
           </div>
         </div>
       </div>
